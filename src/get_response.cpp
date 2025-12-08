e133e93039d02eb57218e0aa8608b6d9578eabc2
Meruem <alex.herlaud99@gmail.com>
2025-12-08 23:23:04 +0100

    new feature & fix (Makefile & bugs) : Create the Makefile for the project.------Change of return into std::string for get_response so that the std::string heap buffer can be used and not a const char* that used a std::string on the stack(crash).------Implement the function setsockot with the flag SO_REUSEADDR for the listening socket so that the binding won't be blocked on TIME_WAIT

--- src/get_response.cpp
+++ src/get_response.cpp
@@ -1,11 +1,12 @@
 #include <fstream>
 
-const char *get_response(const char *file_path)
+std::string get_response(const char *file_path)
 {
     std::ifstream index_fd(file_path);
     std::string line;
     std::string msg = "";
     while(std::getline(index_fd, line))
         msg += line + "\r\n";
-    return (msg.c_str());
+    index_fd.close();
+    return (msg);
 }
