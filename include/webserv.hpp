0d9e6fb4d51f4286c5603c661e03eb3a862be3d7
ldevigne <lucas.devigne@gmail.com>
2026-02-07 14:53:18 +0100

    ready to merge

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -13,6 +13,7 @@
 # include <sstream>
 # include <iostream>
 # include <iomanip>
+# include <cstdlib>
 
 std::string get_response(const char *file_path);
 
@@ -28,22 +29,31 @@ class http_request
 	public:
 		http_request(){};
 		~http_request(){};
-		int		get_method() const;
-		int		get_port() const;
-		int		get_content_length() const;
-		bool	get_keep_alive() const;
+		int			get_method() const;
+		std::string	get_version() const;
+		int			get_port() const;
+		int			get_content_length() const;
+		bool		get_keep_alive() const;
 		std::string get_path_to_send() const;
-		void	set_method(int method);
-		void 	set_method_Get(std::string line);
-		void	set_port(int port);
-		void	set_content_length(const std::string &line);
-		void	set_keep_alive(bool status);
+		void		set_method(int method);
+		void 		set_path(const std::string &path);
+		void 		set_version(const std::string &version);
+		void		set_port(int port);
+		void		set_content_length(const std::string &line);
+		void		set_keep_alive(bool status);
+		void		add_header(const std::string &header, const std::string &value);
+		void		add_body(const std::string &body);
 	private:
-		bool			keep_alive_;
-		int				method_;
-		int				port_;
-		int				content_length_;
-		std::string		path_to_send_;
+		int			id_server_;
+		bool		keep_alive_;
+		int			method_;
+		std::string	version_;
+		int			port_;
+		int			content_length_;
+		std::string	path_to_send_;
+		std::string	body_;
+
+		std::map<std::string, std::string>	headers_;
 };
 
 std::ostream &operator<<(std::ostream &flux, http_request const &obj);
