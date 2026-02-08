b3064af4dfb91303dbbc0ae4529ddf375a7b23ce
Meruem <alex.herlaud99@gmail.com>
2026-02-09 00:00:05 +0100

    new feature (response_request : response code, HTML) : change success code and message for POST(201) and DELETE(204) and implement the response code error 403 forbidden path for DELETE. Add HTML files for delete success, 403 error and clean index.html

--- src/mine/get_response_code.cpp
+++ src/mine/get_response_code.cpp
@@ -14,8 +14,14 @@ void Response::set_response_code_message(int code)
     std::string code_message;
     if (code == 200)
         code_message = "OK";
+    else if (code == 201)
+        code_message = "Created";
+    else if (code == 204)
+        code_message = "No Content";                
     else if (code == 404)
         code_message = "Not Found";
+    else if (code == 403)
+        code_message = "Forbidden";
 
     error_code_ = make_pair(code, code_message);
 }
